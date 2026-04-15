11c19988ad238c140ddf88af6c7247f4ce71640a
Krishnna-code <sharmkrish10@gmail.com>
2026-04-15 22:52:09 +0530

    tiny error

--- Borrow/settings.py
+++ Borrow/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app' ,'static')]
+STATICFILES_DIRS = [(BASE_DIR / "app" / "static")]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_URL = '/media/'
