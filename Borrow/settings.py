d9f2041f35d04d2645a6a8728c33068ab798866f
Krishnna-code <sharmkrish10@gmail.com>
2026-04-15 22:42:25 +0530

    css

--- Borrow/settings.py
+++ Borrow/settings.py
@@ -130,3 +130,4 @@ LOGIN_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+WHITENOISE_MANIFEST_STRICT = False
